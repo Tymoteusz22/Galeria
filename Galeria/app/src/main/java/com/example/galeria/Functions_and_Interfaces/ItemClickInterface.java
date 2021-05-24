155c1b883aa01245d0529ab7506fe313e038b644
TymoteuszXXL <84743380+TymoteuszXXL@users.noreply.github.com>
2021-05-24 22:49:40 +0200

    Delete Galeria directory

--- Galeria/app/src/main/java/com/example/galeria/Functions_and_Interfaces/ItemClickInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.galeria.Functions_and_Interfaces;
-
-public interface ItemClickInterface {
-    void onItemClick(int position);
-    void onLongItemClick(int position);
-}
--- Galeria/app/src/main/java/com/example/galeria/Functions_and_Interfaces/OnMediaListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.galeria.Functions_and_Interfaces;
-
-public interface OnMediaListener {
-    void returnIdx(int index);
-    void longClickPerformed();
-}
